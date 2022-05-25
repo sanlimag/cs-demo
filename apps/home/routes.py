5fbe40fca591628c518b6c0096296fac777f57a2
sanlimag <sanlimg@gmail.com>
2022-05-24 21:44:30 -0400

    multiple small fixes

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -13,7 +13,7 @@ from jinja2 import TemplateNotFound
 @login_required
 def index():
 
-    return render_template('home/index.html', segment='index')
+    return render_template('home/dashboard.html', segment='index')
 
 
 @blueprint.route('/<template>')
