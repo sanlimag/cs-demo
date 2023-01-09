c758344b8b2dd2b93829c9fa00181eeb5a346a0c
sanlimag <sanlimg@gmail.com>
2023-01-09 15:42:33 -0500

    json logging

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -5,7 +5,7 @@ Copyright (c) 2019 - present AppSeed.us
 
 bind = '0.0.0.0:5005'
 workers = 1
-accesslog = '-'
-loglevel = 'error'
+#accesslog = '-'
+loglevel = 'critical'
 capture_output = True
 enable_stdio_inheritance = True
